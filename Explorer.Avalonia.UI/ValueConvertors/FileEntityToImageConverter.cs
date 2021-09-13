a9f3737b7f4528f44cec2d01c4d7dbc26a529f99
Mike <dreamer94mv@gmail.com>
2021-09-13 18:18:22 +0500

    fix bag

--- Explorer.Avalonia.UI/ValueConvertors/FileEntityToImageConverter.cs
+++ Explorer.Avalonia.UI/ValueConvertors/FileEntityToImageConverter.cs
@@ -12,16 +12,23 @@ namespace Explorer.Avalonia.UI
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            //var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
+            //var bitmap = new Bitmap(assets.Open(new Uri("avares://Explorer.Avalonia.UI/Assets/folder_icon.png")));
+
             var drivingImage = new DrawingImage();
             if (value is DirectoryViewModel)
             {
                 Image img = new Image();
              
-                if (App.Current.TryFindResource("FolderIconImage", out object? resource))
+                if (App.Current.TryFindResource("FolderIconImagePng", out object? resource))
                 {
                     if (resource is IImage image)
                     {
                         return image;
+                    }
+                    if (resource is Image image2)
+                    {
+                        return image2.Source;
                     }
                 }
             }
