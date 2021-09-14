5786c6227a3122bed074b506b702dcd6d5566639
Mike <dreamer94mv@gmail.com>
2021-09-14 18:57:34 +0500

    Добавил стили к кнопкам окна
    остался баг с разворачиванием окна

---
+++ WPF/Explorer.WPF.UI/AttachedProperties/Windows.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Windows;
+
+namespace Explorer.WPF.UI
+{
+    internal class Windows
+    {
+        public static readonly DependencyProperty IsActiveProperty = DependencyProperty.RegisterAttached(
+            name: "IsActive", propertyType: typeof(bool), ownerType: typeof(Windows), defaultMetadata: new PropertyMetadata(default(bool)));
+
+        public static void SetIsActive(DependencyObject element, bool value)
+        {
+            element.SetValue(IsActiveProperty, value);
+        }
+
+        public static bool GetIsActive(DependencyObject element)
+        {
+            return (bool)element.GetValue(IsActiveProperty);
+        }
+
+        public static readonly DependencyProperty WindowsStateProperty = DependencyProperty.RegisterAttached(
+            name: "WindowsState", propertyType: typeof(WindowState), ownerType: typeof(Windows), defaultMetadata: new PropertyMetadata(default(WindowState)));
+
+        public static void SetWindowsState(DependencyObject element, WindowState value)
+        {
+            element.SetValue(WindowsStateProperty, value);
+        }
+
+        public static WindowState GetWindowsState(DependencyObject element)
+        {
+            return (WindowState)element.GetValue(WindowsStateProperty);
+        }
+    }
+}
