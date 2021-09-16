f9497e5a30a1bc4ab66b53571c09147811f49f20
mememesha <dreamer94mv@gmail.com>
2021-09-17 00:47:39 +0500

    fix bag style avalonia windows

--- AvaloniaUI/Controls/MikeV.WindowGoogleChrome.Avalonia/Controls/GoogleChromeWindow.cs
+++ AvaloniaUI/Controls/MikeV.WindowGoogleChrome.Avalonia/Controls/GoogleChromeWindow.cs
@@ -1,10 +1,24 @@
 ﻿
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Styling;
+using System;
 
 namespace MikeV.WindowGoogleChrome.Avalonia
 {
-    public class GoogleChromeWindow:Window
+    public class GoogleChromeWindow:Window,IStyleable
     {
+        Type IStyleable.StyleKey => typeof(GoogleChromeWindow);
 
+        public static StyledProperty<object> ToolBarContentProperty = AvaloniaProperty.Register<GoogleChromeWindow, object>(nameof(ToolBarContent));
+        public object ToolBarContent
+        {
+            get => GetValue(ToolBarContentProperty);
+            set => SetValue(ToolBarContentProperty, value);
+        }
+        public GoogleChromeWindow()
+        {
+
+        }
     }
 }
